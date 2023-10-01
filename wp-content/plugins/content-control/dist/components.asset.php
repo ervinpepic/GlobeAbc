d909a12a7354d7a810173d9a1e59129a78314d18
Ervin Pepic <pepic_ervin@hotmail.com>
2023-10-01 16:19:31 +0200

    Updated and bug fixes in contact forms

---
+++ wp-content/plugins/content-control/dist/components.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('content-control-core-data', 'content-control-utils', 'lodash', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-data', 'wp-element', 'wp-i18n', 'wp-primitives'), 'version' => 'dfd69afe8fc25a9b6235');
